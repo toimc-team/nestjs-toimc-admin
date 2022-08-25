1c95ab1be5df900f63a5ecaf7556204ea8b5cc6f
东东 <398713567@qq.com>
2022-08-25 16:17:09 +0800

    feat: [RBAC] 创建用户角色模型

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,12 @@ import { AppModule } from './app.module';
 import { setupSwagger } from './swagger/index';
 import type { NestExpressApplication } from '@nestjs/platform-express';
 
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-
   setupSwagger(app);
   app.useGlobalPipes(
     new ValidationPipe({
