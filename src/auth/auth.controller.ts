bb3f5bd1c2be1b9ac2226f8099ceaf7e555a3854
toimc <brian@toimc.com>
2022-10-16 19:46:17 +0800

    fix: test启动脚本异常、删除无用引用

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,11 +1,4 @@
-import {
-  Body,
-  Controller,
-  HttpCode,
-  HttpStatus,
-  Post,
-  Req,
-} from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthDto } from './dto';
 import { ApiTags } from '@nestjs/swagger';
